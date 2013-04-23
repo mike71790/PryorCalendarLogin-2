5715b037d6a33825070afc564fd53e498a210cf5
Michael Pryor <mike71790@comcast.net>
2013-04-23 11:58:02 -0400

    login

---
+++ PryorCalendarLogin/Models/UserModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace _411Calendar.Models
+{
+    public class UserModel
+    {
+        public string UserID { get; set; }
+        [Display(Name="User Name")]
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        [Display(Name="Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
